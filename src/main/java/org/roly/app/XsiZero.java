4719713431afafd005eb33379420b0ef58716878
bioshark <rollandsov@gmail.comi>
2024-07-31 17:45:13 +0200

    working game

--- src/main/java/org/roly/app/XsiZero.java
+++ src/main/java/org/roly/app/XsiZero.java
@@ -10,21 +10,7 @@ public class XsiZero
 {
     public static void main( String[] args ) {
 
-        Board board = new Board();
         GameService service = new GameService();
-
-        System.out.println(board);
-
-        board.addCell(new Cell(State.X), 1, 3);
-        board.addCell(new Cell(State.O), 3, 2);
-
-        System.out.println(board);
-
-        Optional<Cell> winner = service.whoWon(board);
-        if (winner.isPresent() && !winner.get().equals(new Cell(State.EMPTY))) {
-            System.out.println("The winner is " + winner.get().state());
-        } else {
-            System.out.println("There is no winner yet, keep playing!");
-        }
+        service.play();
     }
 }
--- src/main/java/org/roly/domain/service/GameService.java
+++ src/main/java/org/roly/domain/service/GameService.java
@@ -1,31 +1,12 @@
 package org.roly.domain.service;
 
 import java.util.Optional;
+import java.util.Scanner;
 import org.roly.domain.model.Board;
 import org.roly.domain.model.Cell;
+import org.roly.domain.model.Cell.State;
 
 public class GameService {
-
-    public Optional<Cell> whoWon(Board board) {
-        return isTheGameWonHorizontally(board);
-    }
-
-    private Optional<Cell> isTheGameWonHorizontally(Board board) {
-        Cell currentCell;
-        for (Cell[] row : board.getGrid()) {
-            currentCell = row[0];
-            for (Cell cell : row) {
-                if (!cell.equals(currentCell)) {
-                    currentCell = null;
-                    break;
-                }
-            }
-            if (currentCell != null) {
-                return Optional.of(currentCell);
-            }
-        }
-        return Optional.empty();
-    }
 
     public Optional<Cell> determineWinner(Board board) {
         for (int i = 0; i < board.getGrid().length; i++) {
@@ -37,28 +18,74 @@ public class GameService {
         return Optional.empty();
     }
 
+    public void play() {
+        Board board = new Board();
+        System.out.println(board);
+        Optional<Cell> possibleWinner = Optional.empty();
+        while (determineWinner(board).isEmpty()) {
+            String inputForX = readInput(State.X);
+            board.addCell(new Cell(State.X), Integer.parseInt(inputForX) / 10, Integer.parseInt(inputForX) % 10);
+            System.out.println(board);
+            possibleWinner = determineWinner(board);
+            if (possibleWinner.isPresent()) {
+                break;
+            }
+            String inputForO = readInput(State.O);
+            board.addCell(new Cell(State.O), Integer.parseInt(inputForO) / 10, Integer.parseInt(inputForO) % 10);
+            System.out.println(board);
+            possibleWinner = determineWinner(board);
+            if (possibleWinner.isPresent()) {
+                break;
+            }
+        }
+        System.out.println(board);
+        System.out.println("The Winner is " + possibleWinner.get());
+
+    }
+
+    private String readInput(State state) {
+        System.out.print("Position for " + state);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
     private Optional<Cell> checkNeighbours(Board board, int position) {
-        if (position == 0) {
-            Cell cell = board.getGrid()[0][0];
-            if ((cell.equals(board.getGrid()[0][1]) && cell.equals(board.getGrid()[0][2])) ||
-                    (cell.equals(board.getGrid()[1][0]) && cell.equals(board.getGrid()[2][0])) ||
-                    (cell.equals(board.getGrid()[1][1]) && cell.equals(board.getGrid()[2][2]))) {
-                return Optional.of(cell);
-            }
-        } else if (position == 1) {
-            Cell cell = board.getGrid()[1][1];
-            if ((cell.equals(board.getGrid()[1][2]) && cell.equals(board.getGrid()[3][2])) ||
-                    (cell.equals(board.getGrid()[1][0]) && cell.equals(board.getGrid()[1][2])) ||
-                    (cell.equals(board.getGrid()[2][0]) && cell.equals(board.getGrid()[0][2]))) {
-                return Optional.of(cell);
-            }
-        } else if (position == 2) {
-            Cell cell = board.getGrid()[2][2];
-            if ((cell.equals(board.getGrid()[2][0]) && cell.equals(board.getGrid()[2][1])) ||
-                    (cell.equals(board.getGrid()[1][2]) && cell.equals(board.getGrid()[0][2]))) {
-                return Optional.of(cell);
-            }
+        return switch (position) {
+            case 0 -> evaluateTopLeftCell(board);
+            case 1 -> evaluateCenterCell(board);
+            case 2 -> evaluateBottomRight(board);
+            default -> Optional.empty();
+        };
+    }
+
+    private Optional<Cell> evaluateTopLeftCell(Board board) {
+        Cell cell = board.getGrid()[0][0];
+        if ((cell.equals(board.getGrid()[0][1]) && cell.equals(board.getGrid()[0][2])) ||
+                (cell.equals(board.getGrid()[1][0]) && cell.equals(board.getGrid()[2][0])) ||
+                (cell.equals(board.getGrid()[1][1]) && cell.equals(board.getGrid()[2][2]))) {
+            return Optional.of(cell);
         }
         return Optional.empty();
     }
+
+    private Optional<Cell> evaluateCenterCell(Board board) {
+        Cell cell = board.getGrid()[1][1];
+        if ((cell.equals(board.getGrid()[0][1]) && cell.equals(board.getGrid()[2][2])) ||
+                (cell.equals(board.getGrid()[1][0]) && cell.equals(board.getGrid()[1][2])) ||
+                (cell.equals(board.getGrid()[2][0]) && cell.equals(board.getGrid()[0][2]))) {
+            return Optional.of(cell);
+        }
+        return Optional.empty();
+    }
+
+    private Optional<Cell> evaluateBottomRight(Board board) {
+        Cell cell = board.getGrid()[2][2];
+        if ((cell.equals(board.getGrid()[2][0]) && cell.equals(board.getGrid()[2][1])) ||
+                (cell.equals(board.getGrid()[1][2]) && cell.equals(board.getGrid()[0][2]))) {
+            return Optional.of(cell);
+        }
+        return Optional.empty();
+    }
+
+
 }
